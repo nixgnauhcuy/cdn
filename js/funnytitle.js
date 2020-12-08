7f9a51bf317ffe7284abc4b9bbf31227d670eccf
nixgnauhcuy <nixgnauhcuy@gmail.com>
2020-12-08 20:36:50 +0800

    修改funnytitle.js

--- js/funnytitle.js
+++ js/funnytitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/leave.png");
+		 document.querySelector('[rel="icon"]').setAttribute('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/leave.png");
          document.title = '╭(°A°`)╮ 页面崩溃啦';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/favicon.png");
+		 document.querySelector('[rel="icon"]').setAttribute('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/favicon.png");
          document.title = '(ฅ>ω<*ฅ) 噫又好了';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
