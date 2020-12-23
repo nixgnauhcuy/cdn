bb38222aa543b002c30f003c695cfd02926d46e5
nixgnauhcuy <nixgnauhcuy@gmail.com>
2020-12-23 20:41:17 +0800

    change css

--- js/funnytitle.js
+++ js/funnytitle.js
@@ -1,20 +1,15 @@
 /* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
 window.onload = function() {
   var OriginTitile = document.title;
-  var titleTime;
   document.addEventListener('visibilitychange', function() {
     if(document.hidden) {
       $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/leave.png");
       $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/leave.png");
-      document.title = '╭(°A°`)╮ 页面崩溃啦';
-      clearTimeout(titleTime);
+      document.title = '网页走丢了╭(°A°`)╮';
     } else {
       $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/favicon.png");
       $('[rel="shortcut icon"]').attr('href', "https://cdn.jsdelivr.net/gh/nixgnauhcuy/cdn/image/favicon.png");
-      document.title = 'ฅ>ω<*ฅ) 噫又好了';
-      titleTime = setTimeout(function() {
-        document.title = OriginTitile;
-      }, 2000);
+      document.title = OriginTitile;
 	}
   });
 }
